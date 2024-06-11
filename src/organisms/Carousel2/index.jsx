fff2a7e66034084317bd42a99706d54639439ed6
a-elnawawy <eng.ahmed.elnawawy@gmail.com>
2024-06-11 20:07:48 +0300

    react-responsive-carousel init

---
+++ src/organisms/Carousel2/index.jsx
@@ -0,0 +1,5 @@
+import { Carousel2S } from './style.module.css';
+
+export const Carousel2 = () => {
+  return <div className={Carousel2S}>Carousel2</div>;
+};
--- src/templates/HomeT/index.jsx
+++ src/templates/HomeT/index.jsx
@@ -1,6 +1,7 @@
 import { Container } from 'src/atoms/Container';
 
-import { Carousel } from 'src/organisms/Carousel';
+//import { Carousel } from 'src/organisms/Carousel';
+import { Carousel2 } from 'src/organisms/Carousel2';
 
 import { HomeS } from './style.module.css';
 
@@ -8,7 +9,8 @@ export const HomeT = () => {
   return (
     <div className={HomeS}>
       <Container>
-        <Carousel />
+        {/*<Carousel />*/}
+        <Carousel2 />
       </Container>
     </div>
   );
